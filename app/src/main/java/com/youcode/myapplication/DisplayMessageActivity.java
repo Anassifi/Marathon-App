c360392a4ee1d5bbab9ed42fb0ff3ee609fb007b
Anassifi <abdelhakimnassifi@gmail.com>
2021-05-28 10:34:18 +0100

    Adding second activity
    After user submit form he gets a small presentation about youssoufia and a greeting message

--- app/src/main/java/com/youcode/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/youcode/myapplication/DisplayMessageActivity.java
@@ -12,5 +12,17 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String user_name = intent.getStringExtra(MainActivity.EXTRA_FNAME);
+        String user_lname = intent.getStringExtra(MainActivity.EXTRA_LNAME);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView1 = findViewById(R.id.user_name);
+        TextView textView2 = findViewById(R.id.user_lname);
+        textView1.setText(user_name);
+        textView2.setText(user_lname);
     }
+
 }
